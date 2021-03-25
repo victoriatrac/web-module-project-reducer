08398b7ac13cdcd739bf02d51ba9eac062a3a87e
Victoria Trac <victoriatrac@gmail.com>
2021-03-25 17:44:29 -0600

    1/2 mvp

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,7 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
-
+* The button dispatches the addOne() function onClick.
+* addOne() returns the action ADD_ONE.
+* The reducer's action adds 1 to the state's total and returns the new value.
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CalcButton from './components/CalcButton'
 
 import reducer, { calculateResult, initialState } from './reducers/index'
 
-import addOne from './actions/index'
+import applyNumber from './actions/index'
 
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState)
@@ -39,24 +39,63 @@ function App() {
               <CalcButton
                 value={1}
                 onClick={() => {
-                  dispatch(addOne)
-                  console.log(state.total)
+                  dispatch(applyNumber(1))
                 }}
               />
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton
+                value={2}
+                onClick={() => {
+                  dispatch(applyNumber(2))
+                }}
+              />
+              <CalcButton
+                value={3}
+                onClick={() => {
+                  dispatch(applyNumber(3))
+                }}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton
+                value={4}
+                onClick={() => {
+                  dispatch(applyNumber(4))
+                }}
+              />
+              <CalcButton
+                value={5}
+                onClick={() => {
+                  dispatch(applyNumber(5))
+                }}
+              />
+              <CalcButton
+                value={6}
+                onClick={() => {
+                  dispatch(applyNumber(6))
+                }}
+              />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton
+                value={7}
+                onClick={() => {
+                  dispatch(applyNumber(7))
+                }}
+              />
+              <CalcButton 
+                value={8}
+                onClick={() => {
+                  dispatch(applyNumber(8))
+                }}
+              />
+              <CalcButton 
+                value={9}
+                onClick={() => {
+                  dispatch(applyNumber(9))
+                }}
+              />
             </div>
 
             <div className="row">
